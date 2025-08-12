3c33acf40eaafa98ca1d340d0ccf42cc40ba7dfd
Suhail Alblooshi <suhail@binghzal.com>
2025-08-12 20:24:49 +0400

    refactor: rename kubernetes secret and update values.yaml for Argo CD configuration

--- terraform/30-capmox/main.tf
+++ terraform/30-capmox/main.tf
@@ -38,15 +38,15 @@ resource "kubernetes_namespace" "capmox" {
 
 resource "kubernetes_secret" "capmox_credentials" {
 	metadata {
-		name      = "capmox-credentials"
+		name      = "proxmox-credentials"
 		namespace = kubernetes_namespace.capmox.metadata[0].name
 	}
-	data = {
-		url          = base64encode(var.proxmox_url)
-		tokenID      = base64encode(var.proxmox_token_id)
-		tokenSecret  = base64encode(var.proxmox_token_secret)
-	}
 	type = "Opaque"
+	string_data = {
+		url         = var.proxmox_url
+		tokenID     = var.proxmox_token_id
+		tokenSecret = var.proxmox_token_secret
+	}
 }
 
 # Optional: ProxmoxCluster default for management cluster
